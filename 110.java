cf018b1590489c7ef7d96f6ec2294aa67aafe60b
hexy1998 <hexy1998@outlook.com>
2019-11-12 17:35:46 -0500

    Update 110.java

--- 110.java
+++ 110.java
@@ -7,7 +7,49 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
- 
+
+//Recursion
+
+//Use ResultType to return two value
+class Solution {
+    class ResultType{
+        public boolean isBalanced;
+        public int depth;
+        
+        public ResultType(int depth, boolean isBalanced){
+            this.depth = depth;
+            this.isBalanced = isBalanced;
+        }
+        
+    }
+    
+    private ResultType NOT_BALANCED = new ResultType(-1, false); 
+    
+    public boolean isBalanced(TreeNode root) {
+        return maxDepth(root).isBalanced;
+    }
+    
+    private ResultType maxDepth(TreeNode root){
+        if(root == null)
+            return new ResultType(0, true);
+        
+        ResultType left = maxDepth(root.left);
+        ResultType right = maxDepth(root.right);
+        
+        if(! left.isBalanced || ! right.isBalanced)
+            return NOT_BALANCED;
+        
+        if(Math.abs(left.depth - right.depth) > 1)
+            return NOT_BALANCED;
+        
+        return new ResultType(Math.max(left.depth, right.depth) + 1, true);
+        
+    }
+}
+
+
+
+//General
 class Solution {
     private int NOT_BALANCED = -1; 
     
