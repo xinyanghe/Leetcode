fac97389bbf2f9996cc0e30314cb57fa1eebfa41
hexy1998 <hexy1998@outlook.com>
2019-11-16 21:47:07 -0500

    Create 695.java

---
+++ DFS/695.java
@@ -0,0 +1,19 @@
+class Solution {
+    private int dfs(int[][] grid, int i, int j){
+        if( i >= 0 && i < grid.length && j >= 0 && j< grid[0].length && grid[i][j] == 1){
+            grid[i][j] = 0;
+            return 1 + dfs(grid, i - 1, j)+ dfs(grid, i + 1, j)+ dfs(grid, i, j - 1)+ dfs(grid, i, j + 1);
+        }
+        return 0;
+    }
+    public int maxAreaOfIsland(int[][] grid) {
+        int res = 0;
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == 1)
+                    res = Math.max(res, dfs(grid, i, j));
+            }
+        }
+        return res;
+    }
+}
