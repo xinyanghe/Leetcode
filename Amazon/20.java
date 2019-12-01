a5176fecdecd88bc0dce424d9bc5235f8c858e61
hexy1998 <hexy1998@outlook.com>
2019-12-01 02:48:59 -0500

    Create 20.java

---
+++ Amazon/20.java
@@ -0,0 +1,28 @@
+class Solution {
+    private HashMap<Character, Character> mappings;
+    
+    public Solution() {
+        this.mappings = new HashMap<Character, Character>();
+        this.mappings.put(')', '('); // Notice key is ) and value is (
+        this.mappings.put('}', '{');
+        this.mappings.put(']', '[');
+    }
+    
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        
+        for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+
+      if (this.mappings.containsKey(c)) { //Notice this if to skip " "
+        char topElement = stack.empty() ? '#' : stack.pop();
+        if (topElement != this.mappings.get(c)) {
+          return false;
+        }
+      } else {
+        stack.push(c);
+            } 
+        }
+        return stack.isEmpty();
+    } 
+}
