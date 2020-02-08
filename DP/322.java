c7b27489e0cc670ca6d9a29eafd1bd8495ec8fbd
hexy1998 <hexy1998@outlook.com>
2020-02-08 15:53:01 -0500

    Update 322.java

--- DP/322.java
+++ DP/322.java
@@ -21,3 +21,29 @@ class Solution {
         return dp[amount];
     }
 }
+
+//Recurrsion
+class Solution {
+    private int dp(int[] coins, int amount, int[] count) {
+        if (amount < 0)
+            return -1;
+        if (amount == 0)
+            return 0;
+        if (count[amount - 1] != 0)
+            return count[amount - 1];
+        int min = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            int res = dp(coins, amount - coin, count);
+            if (res >= 0 && res < min)
+                min = res + 1;
+        }
+        count[amount - 1] = (min == Integer.MAX_VALUE) ? -1 : min;
+        return count[amount - 1];
+    }
+    
+    public int coinChange(int[] coins, int amount) {
+        if (amount < 1)
+            return 0;
+        return dp(coins, amount, new int[amount]);
+    }
+}
