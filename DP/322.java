95da73cb9f2ca22c0808f9ab7d4e4e4c5350bebd
hexy1998 <hexy1998@outlook.com>
2020-02-08 15:14:51 -0500

    Create 322.java

---
+++ DP/322.java
@@ -0,0 +1,23 @@
+//Iteration
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if (amount < 1)
+            return 0;
+        int[] dp = new int[amount + 1];
+        int sum = 1;//Notice: start from 1
+        
+        while(sum <= amount) {
+            int min = -1;
+            for (int coin : coins) {
+                if (sum >= coin && dp[sum - coin] != -1) {
+                    int temp = dp[sum - coin] + 1;
+                    if (min < 0 || temp < min)
+                        min = temp;
+                }
+            }
+            dp[sum] = min;
+            sum ++;
+        }
+        return dp[amount];
+    }
+}
