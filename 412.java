ecff8650702e5d6a369b7a7f161fabe11d770429
hexy1998 <hexy1998@outlook.com>
2020-02-14 00:58:50 -0500

    Create 412.java

---
+++ 412.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        if (n == 0)
+            return new ArrayList<>();
+        List<String> res = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0)
+                res.add("FizzBuzz");
+            else if (i % 3 == 0)
+                res.add("Fizz");
+            else if (i % 5 == 0)
+                res.add("Buzz");
+            else
+                res.add(Integer.toString(i));
+        }
+        return res;
+    }
+}
