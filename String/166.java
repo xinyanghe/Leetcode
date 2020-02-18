38f0351074e04a3b7336114b125fd2286dcaf231
hexy1998 <hexy1998@outlook.com>
2020-02-17 20:37:06 -0500

    Create 166.java

---
+++ String/166.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String fractionToDecimal(int numerator, int denominator) {
+        if (numerator == 0)
+            return "0";
+        StringBuilder res = new StringBuilder();
+        res.append(((numerator > 0) ^ (denominator > 0)) ?  "-" : "");
+        long num = Math.abs((long)numerator);
+        long den = Math.abs((long)denominator);
+        
+        res.append(num / den);
+        num %= den;
+        
+        if (num == 0)
+            return res.toString();
+        
+        res.append(".");
+        
+        Map<Long, Integer> frac = new HashMap<>();
+        frac.put(num, res.length());
+        while (num != 0) {
+            num *= 10;
+            res.append(num / den);
+            num %= den;
+            if (frac.containsKey(num)) {
+                res.insert(frac.get(num), "(");
+                res.append(")");
+                return res.toString();
+            }
+            frac.put(num, res.length());
+        }
+        return res.toString();
+    }
+}
