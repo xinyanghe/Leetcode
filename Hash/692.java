6970ef41d2fdf65e89a867cc048ef9c4c66f6a83
hexy1998 <hexy1998@outlook.com>
2020-03-02 14:22:30 -0500

    Update 692.java

--- Hash/692.java
+++ Hash/692.java
@@ -10,3 +10,34 @@ class Solution {
         return candidates.subList(0, k);
     }
 }
+
+
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String w : words) {
+            map.put(w, map.getOrDefault(w, 0) + 1);
+        }
+        List<String>[] bucket = new List[words.length + 1];
+        for (int i = 0; i < bucket.length; i++) {
+            bucket[i] = new ArrayList<>();
+        }
+        
+        for (String key : map.keySet()) {
+            bucket[map.get(key)].add(key);
+        }
+        
+        List<String> res = new ArrayList<>();
+        for (int i = bucket.length - 1; i >= 0; i--) {
+            Collections.sort(bucket[i], (b1, b2) -> b1.compareTo(b2));
+            res.addAll(bucket[i]);
+            if (res.size() > k) {
+                for (int j = res.size() - 1; j >= k; j --) {
+                    res.remove(j);
+                }
+            }
+            if (res.size() == k) break;
+        }
+        return res;
+    }
+}
