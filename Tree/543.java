16aedc1dee91767811d650bd8dbb02da14ba01c9
hexy1998 <hexy1998@outlook.com>
2020-03-17 18:50:24 -0400

    Create 543.java

---
+++ Tree/543.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int res;
+    private int dfs(TreeNode root) {
+        if (root == null) return 0;
+        int l = dfs(root.left);
+        int r = dfs(root.right);
+        res = Math.max(res, l + r + 1);
+        return Math.max(l, r) + 1;
+    }
+    public int diameterOfBinaryTree(TreeNode root) {
+        res = 1;
+        dfs(root);
+        return res - 1;
+    }
+}
